88e2e0ab008ec5d152332cdc3e51346fe4fd5dcb
abdullahkaraboga <abdullahkaraboga@icloud.com>
2022-12-17 22:21:09 +0300

    textfield for new layout

--- LayoutWithSwiftUI/ContentView.swift
+++ LayoutWithSwiftUI/ContentView.swift
@@ -6,22 +6,48 @@ struct ContentView: View {
         return .init(name: tag)
 
     }
+
+    @State var aligmentValue: Int = 1
+    @State var text: String = ""
     var body: some View {
         NavigationStack {
             VStack {
-                TagView(aligment: .center, spacing: 10) {
+                
+                Picker("", selection: $aligmentValue) {
+                    Text("Leading").tag(0)
+                    Text("Center").tag(1)
+                    Text("Trailing").tag(2)
+                }.pickerStyle(.segmented)
+                    .padding(.bottom, 20)
+                TagView(aligment: aligmentValue == 0 ? .leading : aligmentValue == 1 ? .center : .trailing, spacing: 10) {
                     ForEach($tags) { $tag in
-                        Toggle(tag.name, isOn: $tag.isSelected).toggleStyle(.button).buttonStyle(.bordered).tint(.red)
+                        Toggle(tag.name, isOn: $tag.isSelected).toggleStyle(.button).buttonStyle(.bordered).tint(tag.isSelected ? .red : .gray)
                     }
-
+                    
+                }.animation(.interactiveSpring(response: 0.5,dampingFraction: 0.6), value: aligmentValue)
+                
+                HStack{
+                    TextField("Tag", text: $text, axis: .vertical).textFieldStyle(.roundedBorder)
+                        .lineLimit(1...5)
                 }
+                
+                Button("Add"){
+                    withAnimation(.spring()){
+                        tags.append(Tag(name: text))
+                        text = ""
+                    }
+                    
+                }.buttonStyle(.bordered)
+                    .buttonBorderShape(.roundedRectangle(radius: 4))
+                    .tint(.red)
+                .disabled(text == "")}
 
 
 
             }.padding(15).navigationTitle(Text("Layouts"))
         }
     }
-}
+
 
 struct TagView: Layout {
     var aligment: Alignment = .center
@@ -38,17 +64,63 @@ struct TagView: Layout {
     }
 
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        
-        
-        
+
+
+
         var origin = bounds.origin
-        var maxWidth = bounds.width
-        
-        subviews.forEach { view in
+        let maxWidth = bounds.width
+
+        var row: ([LayoutSubviews.Element], Double) = ([], 0.0)
+        var rows: [([LayoutSubviews.Element], Double)] = []
+
+
+        for view in subviews {
             let viewSize = view.sizeThatFits(proposal)
-            view.place(at: origin, proposal: proposal)
-            origin.x += (viewSize.width + spacing)
+            if(origin.x + viewSize.width + spacing) > maxWidth {
+                row.1 = (bounds.maxX - origin.x + bounds.minX + spacing)
+                rows.append(row)
+                row.0.removeAll()
+                origin.x = bounds.origin.x
+                row.0.append(view)
+                origin.x += (viewSize.width + spacing)
+
+            } else {
+                row.0.append(view)
+                origin.x += (viewSize.width + spacing)
+
+            }
         }
+
+        if !row.0.isEmpty {
+            row.1 = (bounds.maxX - origin.x + bounds.minX + spacing)
+            rows.append(row)
+        }
+
+        origin = bounds.origin
+
+        for row in rows {
+            origin.x = (aligment == .leading ? bounds.minX : (aligment == .trailing ? row.1 : row.1 / 2))
+
+            for view in row.0 {
+                let viewSize = view.sizeThatFits(proposal)
+                view.place(at: origin, proposal: proposal)
+                origin.x += (viewSize.width + spacing)
+            }
+            let maxHeight = row.0.compactMap { view -> CGFloat? in
+                return view.sizeThatFits(proposal).height
+            }.max() ?? 0
+
+            origin.y += (maxHeight + spacing)
+
+        }
+
+
+        /*subviews.forEach { view in
+            let viewSize = view.sizeThatFits(proposal)
+
+           
+        }
+         */
 
     }
 
